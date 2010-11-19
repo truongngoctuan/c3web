3db003e66820c1e1ea0f6acb9a570f5ce71d3e1b
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-11-19 19:42:32 +0000

    Demo WCF, cơ bản về security

---
+++ 6. WCF/Code 1 Bank Server Client/BankingClient/BankingClient.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using BankingClient.BankingService;
+using System.ServiceModel;
+using System.Net;
+
+namespace BankingClient
+{
+    public partial class BankingClientForm : Form
+    {
+        BankingServiceClient client;
+        public BankingClientForm()
+        {
+            InitializeComponent();
+            client = new BankingServiceClient();
+        }
+
+        private void btSignIn_Click(object sender, EventArgs e)
+        {
+            string username = tbUsername.Text;
+            string password = tbPassword.Text;
+
+            if (client.Login(username, password))
+            {
+                lbIsAuthenticated.Text = "true";
+                GetCurrentMoney();
+            }
+        }
+
+        private void btSignOut_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Logout();
+            }
+            catch (FaultException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+            lbIsAuthenticated.Text = "false";
+        }
+
+        private void GetCurrentMoney()
+        {
+            double money = client.GetCurrentMoney();
+            lbCurrentMoney.Text = money.ToString();
+        }
+
+        private void btDeposit_Click(object sender, EventArgs e)
+        {
+            double amount = double.Parse(tbMoney.Text);
+            try
+            {
+                client.Deposit(amount);
+                GetCurrentMoney();
+            }
+            catch (FaultException<FaultMessage> ex)
+            {
+                MessageBox.Show(ex.Detail.Message, "Error");
+            }
+        }
+
+        private void btWithdraw_Click(object sender, EventArgs e)
+        {
+            double amount = double.Parse(tbMoney.Text);
+            try
+            {
+                client.Withdraw(amount);
+                GetCurrentMoney();
+            }
+            catch (FaultException<FaultMessage> ex)
+            {
+                MessageBox.Show(ex.Detail.Message, "Error");
+            }
+        }
+    }
+}
---
+++ 6. WCF/Code 1 Bank Server Client/BankingService/IBankingService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Security.Permissions;
+
+namespace BankingService
+{
+    // NOTE: If you change the interface name "IService1" here, you must also update the reference to "IService1" in Web.config.
+    [ServiceContract(SessionMode = SessionMode.Required)]
+    public interface IBankingService
+    {
+        [OperationContract]
+        bool Login(string username, string password);
+
+        [OperationContract]
+        [PrincipalPermission(SecurityAction.Demand, Authenticated = true)]
+        void Logout();
+
+        [OperationContract]
+        //[PrincipalPermission(SecurityAction.Demand, Authenticated = true)]
+        double GetCurrentMoney();
+
+        [OperationContract]
+        [FaultContract(typeof(FaultMessage))]
+        //[PrincipalPermission(SecurityAction.Demand, Authenticated=true)]
+        bool Deposit(double value);
+
+        [OperationContract]
+        [FaultContract(typeof(FaultMessage))]
+        //[PrincipalPermission(SecurityAction.Demand, Authenticated = true)]
+        bool Withdraw(double value);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class Account
+    {
+        [DataMember]
+        public int AccountID { get; set; }
+
+        [DataMember]
+        public string Owner { get; set; }
+
+        [DataMember]
+        public double Money { get; set; }
+    }
+
+    [DataContract]
+    public struct FaultMessage
+    {
+        [DataMember]
+        public string Message { get; set; }
+    }
+}
