b41840dad272e9022567e333769e7eb3c0ab1c04
tntuan0712494 <tntuan0712494@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-11-27 03:22:36 +0000

    bổ sung hàm add script cho các control của javascript (không add trực tiếp trong placeholder head ở trang con đc) cập nhật checkbox

--- 3. ASP.NET Template/Web_c3/Web_c3/App_Themes/Default/jsUserControl.js
+++ 3. ASP.NET Template/Web_c3/Web_c3/App_Themes/Default/jsUserControl.js
@@ -10,3 +10,60 @@ function Init()
 //        $(this).css('-webkit-border-radius', '5px');
 //    });
 }
+
+function InitCheckBox() {
+    //http://www.filamentgroup.com/lab/accessible_custom_designed_checkbox_radio_button_inputs_styled_css_jquery/
+    $(':checkbox').each(function(i) {
+        if ($(this).is('[type=checkbox],[type=radio]')) {
+            var input = $(this);
+
+            // get the associated label using the input's id
+            var label = $('label[for=\'' + input.attr('id') + '\']');
+
+            // necessary for browsers that don't support the :hover pseudo class on labels
+            label.hover(
+				function() {
+				    if (input.is(':checked')) {
+				        label.addClass('checkedHover');
+				    }
+				    else {
+				        label.addClass('hover');
+				    }
+				},
+				function() {
+				    label.removeClass('hover checkedHover');
+				    if (input.is(':checked')) {
+				        label.addClass('checked');
+				    }
+				}
+			);
+            //bind custom event, trigger it, bind click,focus,blur events					
+            input.bind('updateState', function() {
+                if (input.is(':checked')) {
+                    if (input.is(':hover')) {
+                        label.addClass('checkedHover');
+                    }
+                    else {
+                        label.addClass('checked');
+                    }
+                }
+                else {
+                    label.removeClass('checked checkedHover checkedFocus');
+
+                }
+
+            })
+			.trigger('updateState')
+			.click(function() {
+			    $(this).trigger('updateState');
+			})
+			.focus(function() {
+			    label.addClass('focus');
+			    if (input.is(':checked')) {
+			        $(this).addClass('checkedFocus');
+			    }
+			})
+			.blur(function() { label.removeClass('focus checkedFocus'); });
+        }
+    });
+}
