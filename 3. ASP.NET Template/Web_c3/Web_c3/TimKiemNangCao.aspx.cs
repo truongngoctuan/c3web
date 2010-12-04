56f13e6f6f62087b88c43fcb4130442441db2297
hoangminh89 <hoangminh89@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-04 16:43:33 +0000

    Sửa lần 2

--- 3. ASP.NET Template/Web_c3/Web_c3/TimKiemNangCao.aspx.cs
+++ 3. ASP.NET Template/Web_c3/Web_c3/TimKiemNangCao.aspx.cs
@@ -7,11 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Web_c3
 {
-    public partial class TimKiemNangCao : BasePage
+    public partial class TimKiemNangCao : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+
         }
     }
 }
