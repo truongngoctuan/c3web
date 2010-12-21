2f1c9a3aa83144052519ea036128dec325f060bf
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-21 08:17:31 +0000

    Custom metadata - trạm xe, chỗ ngồi

--- trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/CustomMetadata.cs
+++ trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/CustomMetadata.cs
@@ -8,6 +8,7 @@ using System.ComponentModel;
 
 namespace CTLH_C3
 { 
+#region Tuyen xe
     public class TUYEN_XE_Metadata
     {
         [DisplayName("Giá vé")]
@@ -38,7 +39,9 @@ namespace CTLH_C3
     {
 
     }
+#endregion
 
+#region Chuyen xe
     public class CHUYEN_XE_Metadata
     {
         [DisplayName("Giờ khởi hành")]
@@ -78,4 +81,92 @@ namespace CTLH_C3
     {
 
     }
+#endregion
+
+#region Tram xe
+    public class TRAM_XE_Metadata
+    {
+        [DisplayName("Tên trạm")]
+        public object TenTramXe { set; get; }
+
+        [DisplayName("Địa chỉ")]
+        public object DiaChi { set; get; }
+
+        [DisplayName("Hình ảnh")]
+        public object HinhAnh { set; get; }
+
+        [DisplayName("Trưởng trạm")]
+        public object NHAN_VIEN { set; get; }
+
+        [DisplayName("Là trạm đến của")]
+        public object TUYEN_XEs { set; get; }
+
+        [DisplayName("Là trạm đi của")]
+        public object TUYEN_XEs1 { set; get; }
+    }
+
+    [MetadataType(typeof(TRAM_XE_Metadata))]
+    [DisplayName("Trạm xe")]
+    public partial class TRAM_XE
+    {
+
+    }
+#endregion
+
+#region Vi tri cho ngoi
+    public class CHO_NGOI_Metadata
+    {
+        [DisplayName("Vị trí")]
+        public object ViTri { set; get; }
+
+        [DisplayName("Các chỗ")]
+        public object DAT_CHOs { set; get; }
+    }
+
+    [MetadataType(typeof(CHO_NGOI_Metadata))]
+    [DisplayName("Vị trí chỗ ngồi")]
+    public partial class CHO_NGOI
+    {
+
+    }
+#endregion
+
+#region Tinh trang dat cho
+    public class TINH_TRANG_DAT_CHO_Metadata
+    {
+        [DisplayName("Tình trạng")]
+        public object TenTinhTrangDatCho { set; get; }
+
+        [DisplayName("Các chỗ")]
+        public object DAT_CHOs { set; get; }
+    }
+
+    [MetadataType(typeof(TINH_TRANG_DAT_CHO_Metadata))]
+    [DisplayName("Tình trạng đặt chỗ")]
+    public partial class TINH_TRANG_DAT_CHO
+    {
+
+    }   
+#endregion
+
+#region Dat cho
+    public class DAT_CHO_Metadata
+    {
+        [DisplayName("Chỗ ngồi")]
+        public object MaChoNgoi { set; get; }
+
+        [DisplayName("Chuyến xe")]
+        public object CHUYEN_XE { set; get; }
+
+        [DisplayName("Tình trạng")]
+        public object TINH_TRANG_DAT_CHO { set; get; }
+    }
+
+    [MetadataType(typeof(DAT_CHO_Metadata))]
+    [DisplayName("Chỗ ngồi")]
+    public partial class DAT_CHO
+    {
+
+    }
+#endregion
 }
