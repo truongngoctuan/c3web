fa82c1d981036196db27537371904f0264050f1e
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-21 07:12:35 +0000

    Custom Metadata - Tuyến xe

---
+++ 7. Code Dynamic/CTLH_C3/CTLH_C3/CustomMetadata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.DynamicData;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel; 
+
+namespace CTLH_C3
+{ 
+    public class TUYEN_XE_Metadata
+    {
+        [DisplayName("Giá vé")]
+        public object GiaVe { get; set; }
+
+        [DisplayName("Trạm đi")]
+        public object TRAM_XE { get; set; }
+
+        [DisplayName("Trạm đến")]
+        public object TRAM_XE1 { get; set; }
+
+        [DisplayName("Khoảng thời gian đi")]
+        public object KhoangThoiGianDi { get; set; }
+
+        [DisplayName("Tần suất chuyến")]
+        public object TanSuatChuyen { get; set; }
+
+        [DisplayName("Lương tài xế")]
+        public object LuongTaiXe { get; set; }
+    }
+
+    [MetadataType(typeof(TUYEN_XE_Metadata))]
+    [DisplayName("Tuyến xe")]
+    public partial class TUYEN_XE
+    {
+
+    } 
+}
