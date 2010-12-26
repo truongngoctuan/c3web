47317154b631a938afe65a41191458b074a6300b
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-26 13:23:57 +0000

    Thay cho TaiXe\Default

---
+++ 7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/TaiXe_Default.aspx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CTLH_C3
+{
+    public partial class TaiXe_Default : CTLH_C3.Core.BasePage
+    {
+
+    }
+}
