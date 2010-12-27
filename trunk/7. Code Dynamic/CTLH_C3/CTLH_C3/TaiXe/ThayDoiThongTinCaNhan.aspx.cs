1224749ffca0082e2d9c511df3a5645ebd7b623f
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-27 07:11:11 +0000

    Thu dua check authen len Pre_Init

--- trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/ThayDoiThongTinCaNhan.aspx.cs
+++ trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/ThayDoiThongTinCaNhan.aspx.cs
@@ -11,6 +11,27 @@ namespace CTLH_C3
 {
     public partial class ThayDoiThongTinCaNhan :CTLH_C3.Core.BasePage
     {
+        private string _role = "";
+        private string _maNhanVien = "";
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {            
+            if (!IsPostBack)
+            {
+                if (Page.User.Identity.IsAuthenticated)
+                {
+                    _role = Roles.GetRolesForUser(Page.User.Identity.Name)[0];
+                    _maNhanVien = Roles.GetRolesForUser(Page.User.Identity.Name)[1];
+                    if (!_role.Equals("Tài Xế"))
+                        Response.Redirect("/Default.aspx");
+                }
+                else
+                {
+                    Response.Redirect("/Default.aspx");
+                }
+            }
+        }
+
         protected void Page_Init(object sender, EventArgs e)
         {
             DynamicDataManager1.RegisterControl(DetailsView1);
@@ -20,24 +41,10 @@ namespace CTLH_C3
         {
             if (!IsPostBack)
             {
-                if (Page.User.Identity.IsAuthenticated)
-                {
-                    String _role = Roles.GetRolesForUser(Page.User.Identity.Name)[0];
-                    String _maNhanVien = Roles.GetRolesForUser(Page.User.Identity.Name)[1];
-                    if (!_role.Equals("Tài Xế"))
-                        Response.Redirect("/Default.aspx");
-                    else
-                    {
-                        DetailsDataSource.AutoGenerateWhereClause = false;
-                        lblLoaiNhanVien.Text = _role;
-                        DetailsDataSource.Select = "new (MaNhanVien,HoTen, DienThoai, DiaChi, LuongTrongThang, LoaiNhanVien, LOAI_NHAN_VIEN)";
-                        DetailsDataSource.Where = "MaNhanVien==" + _maNhanVien;
-                    }
-                }
-                else
-                {
-                    Response.Redirect("/Default.aspx");
-                }
+                DetailsDataSource.AutoGenerateWhereClause = false;
+                lblLoaiNhanVien.Text = _role;
+                DetailsDataSource.Select = "new (MaNhanVien,HoTen, DienThoai, DiaChi, LuongTrongThang, LoaiNhanVien, LOAI_NHAN_VIEN)";
+                DetailsDataSource.Where = "MaNhanVien==" + _maNhanVien;
             }
         }
     }
