c5fb0712f43462cc04918c3122570e2d29930465
mozilla.com.ltd <mozilla.com.ltd@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-27 07:42:33 +0000

    Check authen o Pre_Init

--- trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/TaiXe_Default.aspx.cs
+++ trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/TaiXe_Default.aspx.cs
@@ -4,11 +4,32 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.DynamicData;
+using System.Web.Security;
 
 namespace CTLH_C3
 {
     public partial class TaiXe_Default : CTLH_C3.Core.BasePage
     {
+        private string _role = "";
+        private string _maNhanVien = "";
 
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Page.User.Identity.IsAuthenticated)
+                {
+                    _role = Roles.GetRolesForUser(Page.User.Identity.Name)[0];
+                    _maNhanVien = Roles.GetRolesForUser(Page.User.Identity.Name)[1];
+                    if (!_role.Equals("Tài Xế"))
+                        Response.Redirect("/Default.aspx");
+                }
+                else
+                {
+                    Response.Redirect("/Default.aspx");
+                }
+            }
+        }
     }
 }
--- trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/XemPhanHoi.aspx.cs
+++ trunk/7. Code Dynamic/CTLH_C3/CTLH_C3/TaiXe/XemPhanHoi.aspx.cs
@@ -11,9 +11,30 @@ namespace CTLH_C3
 {
     public partial class XemPhanHoi : CTLH_C3.Core.BasePage
     {
+        private string _role = "";
+        private string _maNhanVien = "";
+
         protected override void OnPreInit(EventArgs e)
         {
             base.OnPreInit(e);
+        }
+
+        protected void Page_PreInit(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                if (Page.User.Identity.IsAuthenticated)
+                {
+                    _role = Roles.GetRolesForUser(Page.User.Identity.Name)[0];
+                    _maNhanVien = Roles.GetRolesForUser(Page.User.Identity.Name)[1];
+                    if (!_role.Equals("Tài Xế"))
+                        Response.Redirect("/Default.aspx");
+                }
+                else
+                {
+                    Response.Redirect("/Default.aspx");
+                }
+            }
         }
 
         protected void Page_Init(object sender, EventArgs e)
@@ -26,28 +47,14 @@ namespace CTLH_C3
         {
             if (!IsPostBack)
             {
-                if (Page.User.Identity.IsAuthenticated)
-                {
-                    String _role = Roles.GetRolesForUser(Page.User.Identity.Name)[0];
-                    String _maNhanVien = Roles.GetRolesForUser(Page.User.Identity.Name)[1];
-                    if (!_role.Equals("Tài Xế"))
-                        Response.Redirect("/Default.aspx");
-                    else
-                    {
-                        TRAVEL_WEBDataContext dataContext = new TRAVEL_WEBDataContext();
-                        var query = (from c in dataContext.CHUYEN_XEs
-                                     join t in dataContext.TUYEN_XEs on c.MaTuyenXe equals t.MaTuyenXe
-                                     where c.MaTaiXe.Equals(_maNhanVien)
-                                     select new { MaChuyen = c.MaChuyenXe, TramDi = t.TRAM_XE1.TenTramXe, TramDen = t.TRAM_XE.TenTramXe }).Distinct();
-                        GridView1.DataSource = query;
-                        GridView1.DataKeyNames = new string[] { "MaChuyen" };
-                        GridView1.DataBind();                        
-                    }
-                }
-                else
-                {
-                    Response.Redirect("/Default.aspx");
-                }
+                TRAVEL_WEBDataContext dataContext = new TRAVEL_WEBDataContext();
+                var query = (from c in dataContext.CHUYEN_XEs
+                             join t in dataContext.TUYEN_XEs on c.MaTuyenXe equals t.MaTuyenXe
+                             where c.MaTaiXe.Equals(_maNhanVien)
+                             select new { MaChuyen = c.MaChuyenXe, TramDi = t.TRAM_XE1.TenTramXe, TramDen = t.TRAM_XE.TenTramXe }).Distinct();
+                GridView1.DataSource = query;
+                GridView1.DataKeyNames = new string[] { "MaChuyen" };
+                GridView1.DataBind();                       
             } 
         }
         
