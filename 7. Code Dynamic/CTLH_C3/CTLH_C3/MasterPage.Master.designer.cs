c8cc62843b07e2303d727281963428624dc86253
tntuan0712494 <tntuan0712494@181567d3-609d-e88c-c5ba-6855ee8f568d>
2010-12-28 16:47:30 +0000

    điều chỉnh masterpage header: banner, menu, cái xe; mỗi thứ là 1 tấm hình

--- 7. Code Dynamic/CTLH_C3/CTLH_C3/MasterPage.Master.designer.cs
+++ 7. Code Dynamic/CTLH_C3/CTLH_C3/MasterPage.Master.designer.cs
@@ -50,6 +50,15 @@ namespace CTLH_C3 {
         protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
+        /// Menu1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Menu Menu1;
+        
+        /// <summary>
         /// ThemeDataSource control.
         /// </summary>
         /// <remarks>
